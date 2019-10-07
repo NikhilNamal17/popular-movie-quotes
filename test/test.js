61fd77e090a8a01976f75bf455af446488622a1d
NikhilNamal17 <nikhilnamal16@gmail.com>
2019-10-08 02:47:01 +0530

    add test && remove duplicates

--- test/test.js
+++ test/test.js
@@ -1,47 +1,25 @@
 const assert = require("chai").assert;
 const data = require("../data/data.json");
-
-// describe("data.json", () => {
-//     it("should not contain any duplicate quotes", () => {
-//         let initialLength = array.length;
-//         array.forEach(element => {
-//             element["quote"] = element["quote"]
-//                 .replace(/['".,/#!$%^&*;:{}=\-_`~()@]/g, "")
-//                 .toLowerCase();
-//             element["movie"] = element["movie"]
-//                 .replace(/['".,/#!$%^&*;:{}=\-_`~()@]/g, "")
-//                 .toLowerCase();
-//         });
-
-//         let arrayOfString = [];
-//         array.forEach(element => {
-//             arrayOfString.push(JSON.stringify(element));
-//         });
-
-//         const set1 = new Set(arrayOfString);
-//         assert.equal(initialLength, set1.size);
-//     });
-// });
+const _ = require("lodash");
 
 describe("data.json", () => {
-    it("should not contain any duplicate quotes", () => {
-        let initialNumberOfQuotes = data.length;
-        for (let i = 0; i < data.length; i++) {
-            data[i]["quote"] = data[i]["quote"]
-                .replace(/['".,/#!$%^&*;:{}=\-_`~() @]/g, "")
-                .toLowerCase();
-            data[i]["movie"] = data[i]["movie"]
-                .replace(/['".,/#!$%^&*;:{}=\-_`~() @]/g, "")
-                .toLowerCase();
+    it("should not contain duplicates ", () => {
+        function arrUnique(arr) {
+            var cleaned = [];
+            arr.forEach(function(itm) {
+                var unique = true;
+                cleaned.forEach(function(itm2) {
+                    if (_.isEqual(itm, itm2)) unique = false;
+                });
+                if (unique) {
+                    cleaned.push(itm);
+                }
+            });
+            return cleaned;
         }
-        let ArrayOfStringObject = [];
-        for (let i = 0; i < data.length; i++) {
-            ArrayOfStringObject.push(JSON.stringify(data[i]));
-        }
-        let setOfAllObjects = new Set(ArrayOfStringObject);
-        console.log("====================================");
-        console.log(setOfAllObjects);
-        console.log("====================================");
-        assert.equal(initialNumberOfQuotes, setOfAllObjects.size);
+        var output = arrUnique(data);
+        var prevArray = data.length;
+
+        assert.equal(prevArray, output.length);
     });
 });
