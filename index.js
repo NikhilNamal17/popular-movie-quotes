12a882a5f51335b21a406f58776426047856ba95
NikhilNamal17 <nikhilnamal16@gmail.com>
2019-10-21 12:00:48 +0530

    fix bugs

--- index.js
+++ index.js
@@ -67,3 +67,16 @@ module.exports = {
   getQuotesByMovie,
   getQuotesByType
 };
+
+
+const fs = require("fs");
+var out = getQuotesByMovie("joker");
+var jsonContent = JSON.stringify(out);
+fs.writeFile("movie.json", jsonContent, "utf8", function(err) {
+  if (err) {
+    console.log("An error occured while writing JSON Object to File.");
+    return console.log(err);
+  }
+
+  console.log("JSON file has been saved.");
+});
